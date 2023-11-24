88e2ae36584b59f02464b044b8c05efc7c580ef8
thalesfb <15121152+thalesfb@users.noreply.github.com>
2023-11-23 22:15:47 -0300

    feat: create file with code

--- ED2/Codificação de Huffman/src/huffman.go
+++ ED2/Codificação de Huffman/src/huffman.go
@@ -1,104 +1,136 @@
 package main
- 
+
 import (
-    "bufio"
-    "container/heap"
-    "fmt"
-    "log"
-    "os"
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"sort"
 )
- 
-type HuffmanTree interface {
-    Freq() int
-}
- 
-type HuffmanLeaf struct {
-    freq  int
-    value rune
-}
- 
+
 type HuffmanNode struct {
-    freq        int
-    left, right HuffmanTree
+	freq        int
+	order       int
+	value       rune
+	left, right *HuffmanNode
 }
- 
-func (self HuffmanLeaf) Freq() int {
-    return self.freq
-}
- 
+
 func (self HuffmanNode) Freq() int {
-    return self.freq
+	return self.freq
 }
- 
-type treeHeap []HuffmanTree
- 
-func (th treeHeap) Len() int { return len(th) }
+
+func (self HuffmanNode) Order() int {
+	return self.order
+}
+
+type treeHeap []*HuffmanNode
+
+// Implementação das funções da interface heap.Interface
+func (th treeHeap) Len() int {
+	//fmt.Println("my Len")
+	return len(th)
+}
 func (th treeHeap) Less(i, j int) bool {
-    return th[i].Freq() < th[j].Freq()
+	if th[i].Freq() == th[j].Freq() {
+		return th[i].Order() < th[j].Order()
+	}
+	return th[i].Freq() < th[j].Freq()
 }
-func (th *treeHeap) Push(ele interface{}) {
-    *th = append(*th, ele.(HuffmanTree))
+
+func (th *treeHeap) PushWithOrder(ele *HuffmanNode) {
+	index := len(*th)
+	//fmt.Println("Index: ", index)
+	for i, node := range *th {
+		if ele.Freq() < node.Freq() {
+			index = i
+			//fmt.Println("Novo Index: ", index)
+			break
+		}
+	}
+	*th = append((*th)[:index], append([]*HuffmanNode{ele}, (*th)[index:]...)...)
+	// imprimir o elemento que foi inserido e seu elemento da esquerda e direita
+	if index > 0 {
+		//fmt.Printf("Elemento inserido: %c\t%d\t%d\n", ele.value, ele.freq, ele.order)
+		//fmt.Printf("Elemento a esquerda: %c\t%d\t%d\n", (*th)[index-1].value, (*th)[index-1].freq, (*th)[index-1].order)
+		// fmt.Printf("Frequencia:%d\t Simbolo:%c\t Ordem:%d\nFrequencia:%d\t Simbolo:%c\t Ordem:%d \n", ele.Freq(), ele.value, ele.order, (*th)[index-1].Freq(), (*th)[index-1].value, (*th)[index-1].order)
+	}
 }
-func (th *treeHeap) Pop() (popped interface{}) {
-    popped = (*th)[len(*th)-1]
-    *th = (*th)[:len(*th)-1]
-    return
+
+func (th *treeHeap) RemoveFirst() interface{} {
+	if len(*th) == 0 {
+		return nil
+	}
+	primeiroElemento := (*th)[0]
+	*th = (*th)[1:]
+	return primeiroElemento
 }
+
 func (th treeHeap) Swap(i, j int) { th[i], th[j] = th[j], th[i] }
 
-func buildHeap(symFreqs map[rune]int) treeHeap {
-    var trees treeHeap
-    for c, f := range symFreqs {
-        trees = append(trees, HuffmanLeaf{f, c})
-    }
-    heap.Init(&trees)
-    return trees
+func buildTree(trees treeHeap) HuffmanNode {
+	for trees.Len() > 1 {
+		a := trees.RemoveFirst().(*HuffmanNode)
+		b := trees.RemoveFirst().(*HuffmanNode)
+		//fmt.Printf("Frequencia:%d\t Simbolo:%c\t Ordem:%d\nFrequencia:%d\t Simbolo:%c\t Ordem:%d \n", a.Freq(), a.value, a.order, b.Freq(), b.value, b.order)
+		trees.PushWithOrder(&HuffmanNode{a.Freq() + b.Freq(), a.order + b.order, 0, a, b})
+		//fmt.Println("Fila após a inserção:")
+		//printHeap(trees)
+	}
+	return *trees[0] // Retorna a raiz da árvore de Huffman
 }
 
-func buildTree(trees treeHeap) HuffmanTree {
-    for trees.Len() > 1 {
-        a := heap.Pop(&trees).(HuffmanTree)
-        b := heap.Pop(&trees).(HuffmanTree)
-        // imprimir frequências e o simbolo
-        fmt.Printf("%d: %c\t%d: %c\n", a.Freq(), a.(HuffmanLeaf).value, b.Freq(), b.(HuffmanLeaf).value)
-        //fmt.Println(a.Freq(), b.Freq())
-        heap.Push(&trees, HuffmanNode{a.Freq() + b.Freq(), a, b})
-    }
-    return heap.Pop(&trees).(HuffmanTree)
-}
-
+// Imprime a fila de prioridade
 func printHeap(trees treeHeap) {
-    for _, tree := range trees {
-        switch i := tree.(type) {
-        case HuffmanLeaf:
-            fmt.Printf("%c\t%d\n", i.value, i.freq)
-        case HuffmanNode:
-            fmt.Printf("%d\n", i.Freq())
-        }
-    }
+	for _, tree := range trees {
+		fmt.Printf("%c\t%d\t%d\n", tree.value, tree.freq, tree.order)
+	}
 }
 
-// Imprime os códigos de Huffman a partir da raiz da árvore de Huffman. Usa byte[] para armazenar os códigos
-func printCodes(tree HuffmanTree, prefix []byte) {
-    switch i := tree.(type) {
-    case HuffmanLeaf:
-        // Se esse é um nó folha, então ele contém um dos caracteres de entrada, imprime o caractere e seu código de byte[]
-        fmt.Printf("%c\t%d\t%s\n", i.value, i.freq, string(prefix))
-    case HuffmanNode:
-        // Assina 0 para a borda esquerda e recorre
-        prefix = append(prefix, '0')
-        printCodes(i.left, prefix)
-        prefix = prefix[:len(prefix)-1]
- 
-        // Assina 1 para a borda direita e recorre
-        prefix = append(prefix, '1')
-        printCodes(i.right, prefix)
-        prefix = prefix[:len(prefix)-1]
-    }
+// função para construir a codificação de Huffman
+func buildCodes(tree HuffmanNode, prefix []byte, codes map[rune]string) {
+	if tree.value != 0 {
+		// Se é uma folha, adicione o símbolo e seu código ao mapa
+		codes[tree.value] = string(prefix)
+	} else {
+		// Percorre a esquerda adicionando '0' ao prefixo
+		prefix = append(prefix, '0')
+		buildCodes(*tree.left, prefix, codes)
+		prefix = prefix[:len(prefix)-1]
+
+		// Percorre a direita adicionando '1' ao prefixo
+		prefix = append(prefix, '1')
+		buildCodes(*tree.right, prefix, codes)
+		prefix = prefix[:len(prefix)-1]
+	}
+}
+
+// Função auxiliar para inicializar e chamar buildCodes
+func printCodes(tree HuffmanNode) map[rune]string {
+	codes := make(map[rune]string)
+	buildCodes(tree, []byte{}, codes)
+	return codes
+}
+
+// função para salvar a codificação de Huffman em um arquivo
+func save_file(codes map[rune]string, nome_arquivo string) error {
+	// Cria o arquivo
+	arquivo, err := os.Create(nome_arquivo)
+	if err != nil {
+		log.Fatal(err)
+		return err // Retorna o erro se houver algum
+	}
+	defer arquivo.Close()
+
+	// Escreve no arquivo
+	for simbolo, codigo := range codes {
+		arquivo.WriteString(fmt.Sprintf("%c:%s\n", simbolo, codigo))
+	}
+
+	return nil
 }
 
 // função que le o arquivo e retorna um map com os símbolos e suas frequências
-func ler_arquivo(nome_arquivo string) (string, error) {
+func read_file(nome_arquivo string) (string, error) {
 	// Abre o arquivo
 	arquivo, err := os.Open(nome_arquivo)
 	if err != nil {
@@ -121,38 +153,76 @@ func ler_arquivo(nome_arquivo string) (string, error) {
 	}
 	return conteudo, nil
 }
-// função para contar a frequência de cada símbolo no conteúdo
-func conta_simbolos(conteudo string) map[rune]int {
-    // Cria um mapa para armazenar as frequências
-    frequencias := make(map[rune]int)
 
-    // Conta a frequência de cada símbolo
-    for _, simbolo := range conteudo {
-        if simbolo != '\n' {
-            frequencias[simbolo]++
-        }
-    }
-    return frequencias
+func count_symbol_construct_queot(conteudo string) treeHeap {
+	var trees treeHeap
+	frequencias := make(map[rune]HuffmanNode)
+	ordem := 0
+
+	for _, simbolo := range conteudo {
+		if simbolo != '\n' {
+			node, ok := frequencias[simbolo]
+			if !ok {
+				node = HuffmanNode{freq: 1, order: ordem, value: simbolo}
+				frequencias[simbolo] = node
+				ordem++
+			} else {
+				node.freq++
+				frequencias[simbolo] = node
+			}
+		}
+	}
+
+	for _, node := range frequencias {
+		nodeCopy := node                 // Criar uma cópia do node
+		trees = append(trees, &nodeCopy) // Anexar a cópia, não o original
+	}
+
+	sort.Slice(trees, func(i, j int) bool {
+		if trees[i].freq != trees[j].freq {
+			return trees[i].freq < trees[j].freq
+		}
+		return trees[i].order < trees[j].order
+	})
+
+	return trees
+}
+
+// função para calcular a taxa de compressão
+func taxa_compressao(conteudo string, codes map[rune]string) float64 {
+	var tamanho_original, tamanho_comprimido int
+	for _, simbolo := range conteudo {
+		tamanho_original++
+		tamanho_comprimido += len(codes[simbolo])
+	}
+	return float64(tamanho_comprimido) / float64(tamanho_original)
 }
 
 // Driver program to test above functions
 func main() {
-    // caminho do arquivo
-    caminho := "arquivos/arquivo.txt"
-    conteudo, err := ler_arquivo(caminho)
+	// caminho do arquivo
+	caminho := "G:/Meu Drive/Facul/Estrutura de dados/Data-Structure/ED2/Codificação de Huffman/arquivos/arquivo.txt"
+	conteudo, err := read_file(caminho)
 	if err != nil {
 		log.Fatal(err)
 	}
- 
-    symFreqs := conta_simbolos(conteudo)
-    trees := buildHeap(symFreqs)
-    printHeap(trees)
-    huffmanTree := buildTree(trees)
- 
-    // example tree
-    //exampleTree := buildTree(symFreqs)
- 
-    // print out results
-    fmt.Println("SYMBOL\tWEIGHT\tHUFFMAN CODE")
-    printCodes(huffmanTree, []byte{})
+
+	symFreqs := count_symbol_construct_queot(conteudo)
+	fmt.Println("SYMBOL\tWEIGHT\tORDER")
+	printHeap(symFreqs)
+	huffmanTree := buildTree(symFreqs)
+
+	//construir a codificação de Huffman
+	codes := printCodes(huffmanTree)
+
+	// salvar a codificação de Huffman em um arquivo
+	err = save_file(codes, "G:/Meu Drive/Facul/Estrutura de dados/Data-Structure/ED2/Codificação de Huffman/arquivos/codificacao.txt")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// calcular a taxa de compressão
+	taxa := taxa_compressao(conteudo, codes)
+	fmt.Printf("Taxa de compressão: %.2f\n", taxa)
 }
